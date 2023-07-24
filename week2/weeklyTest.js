9269e0bbbab65e6a9918fcb2400dc684ccede6be
adyatma.fulvian <adyatma.fulvian@alpabit.com>
2023-07-24 16:00:18 +0700

    Week 2 [Done]

---
+++ week2/weeklyTest.js
@@ -0,0 +1,87 @@
+/*
+Diberikan sebuah function targetTerdekat(arr) yang menerima satu parameter berupa array yang terdiri dari karakter. Function akan me-return jarak spasi antar karakter 'o' dengan karakter 'x' yang terdekat. Contoh, jika arr adalah ['x', ' ', 'o', ' ', ' ', 'x'], maka jarak terdekat dari 'o' ke 'x' adalah 2. Jika tidak ditemukan 'x' sama sekali, function akan me-return nilai 0.
+*/
+function targetTerdekat(arr) {
+    // you can only write your code here!
+    const indexOfO = arr.indexOf('o');
+    let distance = arr.length;
+
+    if (indexOfO === -1 || arr.indexOf('x') === -1) {
+        return 0;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 'x') {
+            let distanceX = Math.abs(i - indexOfO);
+            if (distanceX < distance) {
+                distance = distanceX;
+            }
+        }
+    }
+
+    return distance;
+}
+
+// TEST CASES
+console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
+console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
+console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
+console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
+
+/*
+Diberikan sebuah function mengelompokkanAngka(arr) yang menerima satu parameter berupa array yang berisi angka-angka. Function akan me-return array multidimensi dimana array tersebut berisikan 3 kategori/kelompok:
+
+kelompok pertama (baris pertama) merupakan angka-angka genap
+kelompok ke-2 (baris ke-2) merupakan angka-angka ganjil
+kelompok ke-3 (baris ke-3) merupakan angka-angka kelipatan 3
+Contoh jika arr inputan adalah [45, 20, 21, 2, 7] maka output: [ [ 20, 2 ], [ 7 ], [ 45, 21 ] ]
+*/
+function mengelompokkanAngka(arr) {
+    // you can only write your code here!
+    let result = [[], [], []];
+
+    for (let i = 0; i < arr.length; i++) {
+        let number = arr[i];
+
+        if (number % 3 === 0) {
+            result[2].push(number);
+        } else if (number % 2 === 0) {
+            result[0].push(number);
+        } else {
+            result[1].push(number);
+        }
+    }
+
+    return result;
+}
+
+// TEST CASES
+console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+console.log(mengelompokkanAngka([])); // [ [], [], [] ]
+
+
+/*
+diberikan sebuah function groupAnimals(animals) yang menerima satu parameter berupa array,
+fungsi ini akan me-return array 2 dimensi
+*/
+function groupAnimals(animals) {
+    // you can only write your code here!
+    const uniqueLetters = new Set(animals.map(animal => animal[0]));
+    let result = [];
+
+    uniqueLetters.forEach(letter => {
+        const group = animals.filter(animal => animal[0] === letter);
+        result.push(group);
+    });
+
+    return result.sort();
+}
+
+// TEST CASES
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak']));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
