221d55ba6212ef9d9fc89e9ae06316bddb9d0711
adyatma.fulvian <adyatma.fulvian@alpabit.com>
2023-08-18 00:01:37 +0700

    prePhase1 [DONE]

---
+++ prePhase1/arrayDimension.js
@@ -0,0 +1,203 @@
+/*
+=====================
+DOUBLE REVERSE ARRAY
+=====================
+
+[INSTRUCTION]
+Terdapat function doubleReverse yang menerima parameter berupa array of string, 
+function ini akan memutar elemen array, string yang berada di dalam elemen array juga akan dibalik atau diputar jika panjang string GENAP
+
+[EXAMPLE]
+input: ['rabu', 'cinta', 'benci', 'masuk', 'nikmat']
+proses: memutar isi array, dan memutar elemen array yang panjang katanya genap
+output: [ 'tamkin', 'masuk', 'benci', 'cinta', 'ubar' ]
+
+[RULES]
+- Function bawaan javascript yang boleh digunakan hanyalah .push dan .length
+- DILARANG menggunakan .reverse()
+*/
+
+function doubleReverse(arr) {
+    //code here 
+    let result = []
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i].length % 2 === 0) {
+            let temp = ''
+            for (let j = arr[i].length - 1; j >= 0; j--) {
+                temp += arr[i][j]
+            }
+            result.push(temp)
+        } else {
+            result.push(arr[i])
+        }
+    }
+    return arr.length > 0 ? result : 'invalid input parameter'
+}
+
+console.log(doubleReverse(['rabu', 'cinta', 'benci', 'masuk', 'nikmat']));
+// [ 'tamkin', 'masuk', 'benci', 'cinta', 'ubar' ]
+console.log(doubleReverse(['aku', 'sayang', 'kamu']));
+// [ 'umak', 'gnayas', 'aku' ]
+console.log(doubleReverse(['pelakor', 'perusak', 'rumah', 'tangga']));
+//[ 'anggnat', 'rumah', 'perusak', 'pelakor' ]
+console.log(doubleReverse([]))
+// invalid input parameter
+
+/*
+==================================
+Array Mastery: Find Not Relative
+==================================
+
+Diberikan sebuah function findNotRelative yang menerima 2 parameter berupa array of number.
+Output adalah array berupa kumpulan angka yang ada di array pertama tetapi tidak ada di array kedua.
+
+Contoh 1:
+input: [ 3, 6, 10, 12, 15 ] dan [ 1, 3, 5, 10, 16 ]
+output: [ 6, 12, 15]
+
+Contoh 2:
+input: [ 10, 20, 36, 59 ] dan [ 5, 10, 15, 59 ]
+output: [20, 36]
+
+RULES:
+ - DILARANG MENGGUNAKAN .map, .filter atau .reduce!
+
+*/
+
+function findNotRelative(arr1, arr2) {
+    //code here
+    let result = []
+
+    for (const item1 of arr1) {
+        let isRelative = false
+        for (const item2 of arr2) {
+            if (item1 === item2) {
+                isRelative = true
+                break
+            }
+        }
+        if (!isRelative) {
+            result.push(item1)
+        }
+    }
+
+    return result
+}
+
+console.log(findNotRelative([3, 6, 10, 12, 15], [1, 3, 5, 10, 16])); // [ 6, 12, 15]
+console.log(findNotRelative([10, 20, 36, 59], [5, 10, 15, 59])); //[20, 36]
+console.log(findNotRelative([1, 2, 3], [2, 1, 3])); //[]
+
+/*
+Minimum Distance Between Greatest Element
+
+Diberikan sebuah function minDistanceBetweenGreatest yang menerima sebuah parameter array of number. Dimana array tersebut akan memiliki nilai terbesar yang kemunculannya bisa lebih dari satu kali. Tentukan minimum jarak antara nilai terbesar satu ke nilai terbesar lainnya.
+
+Contoh 1:
+Input : [ 3, 5, 2, 3, 5, 3, 5 ]
+Output : 2
+
+Karena nilai terbesar dari array tersebut adalah 5 dan indeksnya adalah 1, 4 dan 6
+- indeks 1 ke indeks 4, jaraknya adalah 3
+- indeks 4 ke indeks 6, jaraknya adalah 2 
+
+Jadi minimum jarak adalah 2
+
+Contoh 2:
+Input : [ 1, 1, 1, 1, 1, 1 ]
+Output : 1
+
+
+Karena nilai terbesar dari array tersebut adalah 1 dan indeksnya adalah 0, 1, 2, 3, 4 dan 5
+- indeks 0 ke indeks 1, jaraknya adalah 1
+- indeks 1 ke indeks 2, jaraknya adalah 1
+- indeks 2 ke indeks 3, jaraknya adalah 1
+- indeks 3 ke indeks 4, jaraknya adalah 1
+- indeks 4 ke indeks 5, jaraknya adalah 1
+
+Jadi minimum jarak adalah 1
+
+RULE:
+ - Dilarang menggunakan .map, .filter dan .reduce
+
+*/
+
+function minDistanceBetweenGreatest(arr) {
+    // your code here
+    let max = -1;
+    let arrMax = [];
+    let minDistance = arr.length;
+
+    for (const index in arr) {
+        if (arr[index] > max) {
+            max = arr[index];
+            arrMax = [index];
+        } else if (arr[index] === max) {
+            arrMax.push(index);
+        }
+    }
+
+    if (arrMax.length === 1) {
+        return 0;
+    }
+
+    for (let i = 0; i < arrMax.length - 1; i++) {
+        const distance = arrMax[i + 1] - arrMax[i];
+        if (distance < minDistance) {
+            minDistance = distance;
+        }
+    }
+
+    return minDistance;
+}
+
+console.log(minDistanceBetweenGreatest([3, 5, 2, 3, 5, 3, 5])); //2
+console.log(minDistanceBetweenGreatest([1, 1, 1, 1, 1, 1])); //1
+console.log(minDistanceBetweenGreatest([7, 8, 5, 2, 1, 1])); //0
+
+/*
+==================================
+Array Mastery: Average Length Word
+==================================
+Nama:________
+[INSTRUKSI]
+Disediakan sebuah kalimat. Function averageLengthWord akan menerima satu parameter berupa string
+yang berisikan kalimat tersebut, dan akan mendapatkan rata-rata jumlah huruf dari setiap kata,
+kemudian mengembalikan nilai berupa array of string yang berisikan kata mana saja yang jumlahnya
+sama dengan rata-rata jumlah kata.
+
+NOTE:
+ - Spasi tidak dihitung
+ - Jika hasil rata-rata adalah desimal maka dibulatkan
+
+[CONTOH]
+input (kalimat): Do you want to become a great coder.
+rata-rata panjang kata dalam kalimat: 3
+output: ['you']
+
+input (kalimat): You dont know what you have until you lose it!.
+rata-rata panjang kata dalam kalimat: 4
+output: ['dont', 'know', 'what', 'have', 'lose']
+*/
+
+function averageLengthWord(words) {
+    //code here
+    const regex = /[^A-Za-z0-9]+/g
+
+    let result = []
+    const allWord = words.replace(regex, '')
+    const withoutSpace = words.split(' ')
+    const averageWord = Math.round(allWord.length / withoutSpace.length)
+
+    for (const item of withoutSpace) {
+        if (item.length === averageWord) {
+            result.push(item)
+        }
+    }
+
+    return result
+}
+
+console.log(averageLengthWord('Do you want to become a great coder ?')); // ['you']
+console.log(averageLengthWord('You dont know what you have until you lose it!')); // [ 'dont','know','what','have','lose']
+console.log(averageLengthWord('I am diligent')); // []
